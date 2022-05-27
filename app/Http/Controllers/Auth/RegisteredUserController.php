7efa244d079cfe9fef045cb6b980736744cc3573
Andi Alifsyah <andyalyfsyah4@gmail.com>
2022-05-27 21:29:39 +0700

    username login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'username' => ['required|string|max:255'],
+            'username' => ['required','string','max:255'],
         ]);
 
         $user = User::create([
